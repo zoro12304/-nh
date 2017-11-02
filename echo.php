3184773ed920238c091716da63c1823777d9d337
truong12304 <33325725+truong12304@users.noreply.github.com>
2017-11-03 00:09:25 +0700

    Add files via upload

--- echo.php
+++ echo.php
@@ -4,6 +4,6 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
 
 use zoro12304/Chatfuel;
 
-if (isset($_GET['msg'])) && ! empty($_GET['msg']) {
+if (isset($_GET['msg']) && ! empty($_GET['min'])) {
 	(new Chatfuel())->sendText($_GET['msg']);
 }
